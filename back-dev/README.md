40ad0dd15ca161a72700a606a001622ac9317512
mohammad <diba6122@diba6122s-MacBook-Pro.local>
2016-08-05 22:53:00 -0700

    body-parser is added.

--- back-dev/README.md
+++ back-dev/README.md
@@ -1,1 +1,9 @@
+# Running server
 Run Local Server ==> node app.js
+
+#API Doc
+1. `http://jabeja.com/api/ship/request`
+2. `:sparkles:`
+3. `:-1:`
+4. `:+1:`
+5. `:clap:`
--- back-dev/app.js
+++ back-dev/app.js
@@ -1,7 +1,26 @@
-var conf = require("./config"),
-    express = require("express"),
-    app = express();
+var conf = require('./config'),
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
 
-app.listen(conf.devport, function() {
-  console.log("Express server is running on localhost: " + conf.devport);
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// setup the server port number
+var port = process.env.PORT || conf.devport;
+
+// we are using Express router for routing between APIs
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.json({message: "Test Repsonse."});
 });
+
+// all of our APIs are prefixed with "jabeja/api"
+// Example: http://jabeja.com/jabeja/api/getuser
+app.use('jabeja/api', router);
+
+app.listen(port);
+console.log("Server is running on port: " + port);
