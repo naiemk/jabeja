a247d5f5bb820111a0061a69016958c4b6ed4e8d
mohammad <diba6122@diba6122s-MacBook-Pro.local>
2016-08-16 23:55:33 -0700

    Facebook link is added.

--- front-end/ui/searchresults.js
+++ front-end/ui/searchresults.js
@@ -3,14 +3,16 @@
   angular.module("jabeja").component('searchResults', {
     template : `
       <div class="container">
-        <div class="row space" ng-repeat="item in ctrl.results | orderBy: '-subText'">
+        <div class="row space" ng-repeat="trip in ctrl.results | orderBy: '-deliveryDate'">
           <div class="col col-sm-12">
             <div class="card">
-              <img class="card-img-top" src="{{item.img}}" alt="Facebook image">
+              <img class="card-img-top" src="{{trip.img}}" alt="Facebook image">
               <div class="card-block">
-                <h4 class="card-title">{{item.text}}</h4>
-                <p class="card-text">{{item.subText}}</p>
-                <a href="mailto:{{item.contact}}" ng-show="ctrl.isLoggedIn" class="btn btn-primary">Contact</a>
+                <h4 class="card-title">{{trip.userName}}</h4>
+                <p class="card-text">{{trip.deliveryDate}}</p>
+                <a href="{{trip.facebookLink}}">Facebook Page</a>
+                <br/><br/>
+                <a href="mailto:{{trip.contact}}" ng-show="ctrl.isLoggedIn" class="btn btn-primary">Contact</a>
                 <a href="#" ng-hide="ctrl.isLoggedIn" class="btn btn-primary">Log-in to Contact</a>
               </div>
             </div>
@@ -49,8 +51,9 @@
                   ctrl.results = result.data.map(function(item) {
                     var searchItem = {
                       img : item.userImg || "https://randomuser.me/api/portraits/thumb/women/80.jpg",
-                      text : item.name,
-                      subText: ctrl.toDate(item.finishDate),
+                      userName : item.name,
+                      deliveryDate: ctrl.toDate(item.finishDate),
+                      facebookLink: "https://facebook.com/" + item.userId,
                       contact: item.email
                     };
                     return searchItem;
